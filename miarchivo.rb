1c56e93b7a8278b4b1b0bc9ba57ed6a3b7a99add
uracilo <benjamin.casazza@gmail.com>
2016-10-14 20:22:19 -0500

    mi primer ruby

---
+++ miarchivo.rb
@@ -0,0 +1,9 @@
+# aqui imprime como te llamas
+puts "como te llamas"
+# aqui obtienes la variable y le quitas el salto de linea con el chomp
+nombre=gets.chomp
+# ahora con la edad
+puts "cuale es tu edad"
+edad=gets.chomp
+# ahroa concatenas las respuestas
+puts nombre+" "+edad
